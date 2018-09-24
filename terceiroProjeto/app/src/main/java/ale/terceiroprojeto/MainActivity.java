830c5586e55d35f6f9317977f43b8ecc3876619a
alexandrespmg <alexandrespmg@hotmail.com>
2018-09-24 17:29:26 -0300

    Finished code. - Crazy Layout, dont worry

--- terceiroProjeto/app/src/main/java/ale/terceiroprojeto/MainActivity.java
+++ terceiroProjeto/app/src/main/java/ale/terceiroprojeto/MainActivity.java
@@ -2,6 +2,11 @@ package ale.terceiroprojeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,78 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selectedOption(String userChoise){
+        ImageView appChoiceImage = findViewById(R.id.imageView);
+
+        String[] options = {
+                "Pedra",
+                "Papel",
+                "Tesoura"
+        };
+
+        int randomNumber = new Random().nextInt(3);
+        String appChoice = options[randomNumber];
+
+        switch (appChoice){
+            case "Pedra":
+                appChoiceImage.setImageResource(R.drawable.pedra);
+                break;
+            case "Papel":
+                appChoiceImage.setImageResource(R.drawable.papel);
+                break;
+            case "Tesoura":
+                appChoiceImage.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        this.setWinner(userChoise, appChoice);
+
+    }
+
+    public void selectedTesoura(View view){
+        this.selectedOption("Tesoura");
+    }
+
+    public void selectedPedra(View view){
+        this.selectedOption("Pedra");
+    }
+
+    public void selectedPapel(View view){
+        this.selectedOption("Papel");
+    }
+
+    public void setWinner(String userChoice, String appChoice){
+
+        TextView textResult = findViewById(R.id.textResult);
+        String textToDisplay;
+
+        if( (userChoice == "Pedra" && appChoice == "Tesoura") ||
+            (userChoice == "Papel" && appChoice == "Pedra") ||
+            (userChoice == "Tesoura" && appChoice == "Papel")){
+
+            textToDisplay = "Você ganhou! Parabéns :)";
+
+            textResult.setText( textToDisplay);
+
+        }else if((appChoice == "Pedra" && userChoice == "Tesoura") ||
+                (appChoice == "Papel" && userChoice == "Pedra") ||
+                (appChoice == "Tesoura" && userChoice == "Papel")){
+
+            textToDisplay = "Você perdeu! Tente Novamente :(";
+
+            textResult.setText( textToDisplay);
+
+        }else{
+
+            textToDisplay = "Vocês empataram!! :o";
+
+            textResult.setText( textToDisplay);
+
+        }
+
+
+    }
+
+
 }
