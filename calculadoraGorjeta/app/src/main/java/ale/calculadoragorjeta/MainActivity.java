c5dc91e506dc21711c919bd8c147367f26bdf778
alexandrespmg <alexandrespmg@hotmail.com>
2018-09-27 09:12:37 -0300

    First commit

---
+++ calculadoraGorjeta/app/src/main/java/ale/calculadoragorjeta/MainActivity.java
@@ -0,0 +1,13 @@
+package ale.calculadoragorjeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
