87466dd288f37d463221cd92e3a939b8989851e4
alexandrespmg <alexandrespmg@hotmail.com>
2018-10-05 10:27:39 -0300

    Data sending to second activity

--- TwoActivities/app/src/main/java/ale/twoactivities/FirstActivity.java
+++ TwoActivities/app/src/main/java/ale/twoactivities/FirstActivity.java
@@ -1,13 +1,30 @@
 package ale.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        editText = findViewById(R.id.editText_Main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("text", editText.getText().toString());
+
+        intent.putExtra("FirstActivity", bundle);
+        startActivity(intent);
     }
 }
